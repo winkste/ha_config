dcd8aff28478e366ebef8a2a39033223a76fd929
Stephan Wink <stephan_wink@web.de>
2022-02-18 06:19:59 +0100

    reworked alarm_control app

--- src/appdaemon/apps/alarm_controller.py
+++ src/appdaemon/apps/alarm_controller.py
@@ -22,6 +22,8 @@
 # Args:
 #
 # alarm_offset = Timeout after motion detected to start alarm sequence player
+# away_listener = List of binary sensors that shall trigger alarm if alarm control is in state away
+# night_listener = List of binary sensors triggering the alarm in state night
 # 
 # Signals:
 # alarm_control_panel.ha_alarm:
@@ -35,32 +37,50 @@
 # Version 1.0:
 #   Initial version of app
 
+ALARM_PANEL = "alarm_control_panel.ha_alarm"
+MULTI_LIGHT_SZENES = ["scene.motion_light_red", "scene.motion_light_blue"]
+SZENES_OFF = ["scene.motion_light_blue", "scene.motion_light_off", "scene.all_alarm_lights_off"]
+SINGLE_ALARM_LIGHTS = "scene.all_alarm_lights"
+
 import hassapi as hass
 
 class AlarmCtrl(hass.Hass):
     alarm_timer = None
     alarm_offset = 30
     alarm_activated = False
-    scene_timer_red = None
-    scene_timer_blue = None
+    scene_timer = None
+    away_listener = None
+    night_listener = None
+    multi_light_scene_idx = 0
 
     def initialize(self):
         '''
             Initialization function of class.
         '''
-        # handle arguments
+        # handle offset argument for timeout after motion detected
         if "alarm_offset" in self.args:
             self.alarm_offset = self.args["alarm_offset"]
         else:
             self.alarm_offset = 30
         
         # listen to alarm control panel for state changes armed_away and disarmed
-        self.listen_state(self._alarm_control, "alarm_control_panel.ha_alarm")
+        self.listen_state(self._alarm_control, ALARM_PANEL)
 
-        # listen to all motion and door/window sensors that can trigger the alarm
-        self.listen_state(self._motion_detected, "binary_sensor.dev72_motion", new = "on")
+        # argument list for listeners used when away
+        if "away_listener" in self.args:
+            self.away_listener = self.args["away_listener"]
+            for entity in self.away_listener:
+                self.listen_state(self._away_alarm_listener, entity, new = "on")
+        else:
+            self.log("No parameters found for away alarm")
 
-        # can we use multiple callbacks for the same entity here? one for night and one for away?
+        # argument list for listeners used when night
+        if "night_listener" in self.args:
+            self.night_listener = self.args["night_listener"]
+            for entity in self.night_listener:
+                self.listen_state(self._night_alarm_listener, entity, new = "on")
+        else:
+            self.log("No parameters found for night alarm")           
 
         self.log("alarm controller initialized...")
 
@@ -84,27 +104,35 @@ class AlarmCtrl(hass.Hass):
         '''
             Function that stops all timer and turns light of
         '''
+        self.alarm_activated = False
         if None != self.alarm_timer:
             self.cancel_timer(self.alarm_timer)
-        self.turn_on("scene.motion_light_blue")
-        self.turn_on("scene.motion_light_off")
-        if None != self.scene_timer_red:
-            self.cancel_timer(self.scene_timer_red)
-        if None != self.scene_timer_blue:
-            self.cancel_timer(self.scene_timer_blue)
-        self.alarm_activated = False
+        for scene in SZENES_OFF:
+            self.turn_on(scene)
+        if None != self.scene_timer:
+            self.cancel_timer(self.scene_timer)   
         self.log("alarm controller alarm sequence deactivated")
-
-    def _motion_detected(self, entity, attribute, old, new, kwargs):
-        '''
-            Callback function for motion, or door/window open detected.
-        '''
-        if self.get_state("alarm_control_panel.ha_alarm") == "armed_away":
-            if False == self.alarm_activated:
-                self.log(f"alarm controller motion detected by: binary_sensor.dev72_motion, start alarm sequence")
-                self.alarm_activated = True
-                self.alarm_timer = self.run_in(self._start_alarm, self.alarm_offset)
     
+    def _away_alarm_listener(self, entity, attribute, old, new, kwargs):
+        '''
+            Callback for all sensors detecting event at "armed_away"
+        '''
+        if self.get_state(ALARM_PANEL) == "armed_away":
+            if False == self.alarm_activated:
+                self.log(f"alarm controller away listener, event detected by: {entity}, start alarm sequence")  
+                self.alarm_activated = True
+                self.alarm_timer = self.run_in(self._start_alarm, self.alarm_offset) 
+
+    def _night_alarm_listener(self, entity, attribute, old, new, kwargs):
+        '''
+            Callback for all sensors detecting event at "night_arm"
+        '''
+        if self.get_state(ALARM_PANEL) == "night_arm":
+            if False == self.alarm_activated:
+                self.log(f"alarm controller away listener, event detected by: {entity}, start alarm sequence")  
+                self.alarm_activated = True
+                self.alarm_timer = self.run_in(self._start_alarm, self.alarm_offset) 
+     
     def _start_alarm(self, kwargs):
         '''
             Callback function for alarm start timer.
@@ -112,24 +140,16 @@ class AlarmCtrl(hass.Hass):
         if None != self.alarm_timer:
             self.cancel_timer(self.alarm_timer)
         self.log("alarm controller alarm started")
-        self.turn_on("scene.motion_light_red")
-        self.scene_timer_red = self.run_in(self._activate_scene_red, 2)
-        self.scene_timer_blue = self.run_in(self._activate_scene_blue, 1)
+        self.turn_on(SINGLE_ALARM_LIGHTS)
+        self.turn_on(MULTI_LIGHT_SZENES[self.multi_light_scene_idx])
+        self.multi_light_scene_idx = (self.multi_light_scene_idx + 1) % len(MULTI_LIGHT_SZENES)
+        self.scene_timer = self.run_in(self._toggle_multi_light, 1)
     
-    def _activate_scene_red(self, kwargs):
+    def _toggle_multi_light(self, kwargs):
         '''
-            This function is used to switch the multi color lights to red.
-        '''
-        if True == self.alarm_activated:
-            self.scene_timer_red = self.run_in(self._activate_scene_red, 1)
-            self.turn_on("scene.motion_light_red")
-
-    def _activate_scene_blue(self, kwargs):
-        '''
-            This function is used to switch the multi color lights to blue.
+            This function toggles the multi light szenes
         '''
         if True == self.alarm_activated:
-            self.scene_timer_blue = self.run_in(self._activate_scene_blue, 1)
-            self.turn_on("scene.motion_light_blue")
-
-
+            self.turn_on(MULTI_LIGHT_SZENES[self.multi_light_scene_idx])
+            self.multi_light_scene_idx = (self.multi_light_scene_idx + 1) % len(MULTI_LIGHT_SZENES)
+            self.scene_timer = self.run_in(self._toggle_multi_light, 1)
